bc9d091150e0915d454faa94e628b02efd448e2e
JaxonVoss <109998185+JaxonVoss@users.noreply.github.com>
2024-05-16 14:26:14 -0500

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,35 @@
+package com.company;
+
+import java.util.Scanner;
+import java.util.Random;
+
+public class Main {
+    private static int monkeyTypewriter(String monkeysGoal){
+        Random tempRandom = new Random();
+        int counter = 0;
+
+        for(int i = 0; i < monkeysGoal.length(); i++){
+            char tempChar = monkeysGoal.charAt(i);
+            int randomInt = tempRandom.nextInt(256);
+            if(randomInt != (int)tempChar){
+                i--;
+            }
+            counter++;
+        }
+
+        return counter;
+    }
+
+    public static void main(String[] args) {
+        Scanner myObj = new Scanner(System.in);
+
+        System.out.print("Type your text here (Do not press enter until done): ");
+        String monkeysGoal = myObj.nextLine();
+
+        int counter = monkeyTypewriter(monkeysGoal);
+
+        long secondsDisplay = counter % 60;
+        long elapsedMinutes = counter / 60;
+        System.out.println("Total time the monkey needed on the typewriter for your text: " + elapsedMinutes + ":" + secondsDisplay);
+    }
+}
